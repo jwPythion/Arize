41bf70b5783ba2c090b2719fbb29b93517cb5d6c
jwpythion <jaymesjohnson@me.com>
2018-08-22 11:23:20 -0700

    updated with weather logic

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Weather extends Component {
+    render() {
+        return (<h1>Hello I'm the weather</h1>)
+    }
+}
+
+
+
+
+export default Weather; 
