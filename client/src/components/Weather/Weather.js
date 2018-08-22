680603a94cf210cc575c0f6c3149febaa40f95bb
jwpythion <jaymesjohnson@me.com>
2018-08-22 12:10:36 -0700

    WIP for weathe component

--- client/src/components/Weather/Weather.js
+++ client/src/components/Weather/Weather.js
@@ -1,8 +1,63 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import "./Weather.css";
 
 class Weather extends Component {
+
+    state = {
+        city: ""
+    }
+
+    handleFormInput = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    getWeather = (event) => {
+        event.preventDefault();
+        console.log(this.state.city);
+    }
+
     render() {
-        return (<h1>Hello I'm the weather</h1>)
+        return (
+            <div>
+                <h1>Retirement Environment Weather Environment</h1>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                        </div>
+                        <div
+                        >
+                            <form
+                                className="navbar-form navbar-right"
+                                role="search"
+                                onSubmit={this.getWeather}
+                            >
+                                <div className="form-group">
+                                    <input
+                                        id="city"
+                                        name="city"
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Enter City"
+                                        value={this.state.city}
+                                        onChange={this.handleFormInput}
+                                    />
+                                </div>
+                                <button type="submit" className="btn btn-default">
+                                    Get Forecast
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="jumbotron" />
+                    <div id="forecast" className="row" />
+                </div>
+            </div>
+        )
     }
 }
 
