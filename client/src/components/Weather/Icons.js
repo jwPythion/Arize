2b20b16dcacf6fcc251bfe9705766ab6e2147ee5
cutesd <kimberly@kimberlycooper.me>
2018-08-24 13:46:14 -0700

    weather icons default not found

--- client/src/components/Weather/Icons.js
+++ client/src/components/Weather/Icons.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Images from '../../weather-icons.json';
 
 var findImage = val => {
-    const img = Images.filter(element => element.name === val.toLowerCase());
+    let img = Images.filter(element => element.name === val.toLowerCase());
+    if (!img) img = Images.filter(element => element.name === "not found");
     return img[0].icon;
 }
 
 export const Icon = props => (
-
     <img src={findImage(props.cast)} className="img-fluid" alt="weather icon" />
 );
 
